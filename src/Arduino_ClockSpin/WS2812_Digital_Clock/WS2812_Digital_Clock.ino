864584e989248a35dd0fd6cf4398f8c83f039c63
Sam-567 <sam.chapman567@gmail.com>
2019-04-27 15:22:25 +1000

    digital clock changes

--- src/Arduino_ClockSpin/WS2812_Digital_Clock/WS2812_Digital_Clock.ino
+++ src/Arduino_ClockSpin/WS2812_Digital_Clock/WS2812_Digital_Clock.ino
@@ -2,7 +2,7 @@
 #include "RTClib.h"
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
-#define NUMPIXELS 12
+#define NUMPIXELS 28
 
 int Numbers[][5][3] = {
 { { 1, 1, 1},
@@ -82,7 +82,7 @@ int Numbers[][5][3] = {
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 RTC_DS1307 RTC;
-int standard = 540;
+int standard = 530;
 int tolerance = 8;
 int readPin = A0;
 
@@ -92,11 +92,12 @@ void setup() {
   pixels.begin();
   
   int i = 0;
-  while (i < 12) {
+  while (i < 28) {
     pixels.setPixelColor(i, pixels.Color(0,0,0));
     i++;
   }
-  pixels.show;
+  pixels.show();
+  delay(500);
   pinMode(readPin, INPUT);
   Serial.begin(9600);
 }
@@ -131,43 +132,39 @@ void loop() {
   }
   for (int i = 2; i >= 0; i--) {
     for ( int pin = 0; pin < 5; pin++ ) {
-      pixels.setPixelColor(pin*2 + 2, Numbers[LowMin][pin][i] * pixels.Color(0,150,0));
-      pixels.setPixelColor(pin*2 + 1, Numbers[LowMin][pin][i] * pixels.Color(0,150,0));
+      pixels.setPixelColor(pin, Numbers[LowMin][pin][i] * pixels.Color(0,150,0));
     }
-    pixels.show;
+    pixels.show();
     delay(2);
   }
 
   for ( int pin = 0; pin < 12; pin++ ) {
       pixels.setPixelColor(pin, 0);
   }
-  pixels.show;
+  pixels.show();
   delay(2);
 
   for (int i = 2; i >= 0; i--) {
     for ( int pin = 0; pin < 5; pin++ ) {
-      pixels.setPixelColor(pin*2 + 2, Numbers[HighMin][pin][i] * pixels.Color(0,150,0));
-      pixels.setPixelColor(pin*2 + 1, Numbers[HighMin][pin][i] * pixels.Color(0,150,0));
+      pixels.setPixelColor(pin,  pixels.Color(0, Numbers[HighMin][pin][i] * 150,0));
     }
-    pixels.show;
+    pixels.show();
     delay(2);
   }
 
   for (int i = 2; i >= 0; i--) {
     for ( int pin = 0; pin < 5; pin++ ) {
-      pixels.setPixelColor(pin*2 + 2, Numbers[10][pin][i] * pixels.Color(0,150,0));
-      pixels.setPixelColor(pin*2 + 1, Numbers[10][pin][i] * pixels.Color(0,150,0));
+      pixels.setPixelColor(pin, pixels.Color(0, Numbers[10][pin][i] * 150,0));
     }
-    pixels.show;
+    pixels.show();
     delay(2);
   }
 
   for (int i = 2; i >= 0; i--) {
     for ( int pin = 0; pin < 5; pin++ ) {
-      pixels.setPixelColor(pin*2 + 2, Numbers[LowHour][pin][i] * pixels.Color(0,150,0));
-      pixels.setPixelColor(pin*2 + 1, Numbers[LowHour][pin][i] * pixels.Color(0,150,0));
+      pixels.setPixelColor(pin, pixels.Color(0, Numbers[LowHour][pin][i] * 150,0));
     }
-    pixels.show;
+    pixels.show();
     delay(2);
   }
 
@@ -175,15 +172,14 @@ void loop() {
     for ( int pin = 0; pin < 12; pin++ ) {
       pixels.setPixelColor(pin, 0 );
     }
-    pixels.show
+    pixels.show();
     delay (2);
 
     for (int i = 2; i >= 0; i--) {
       for ( int pin = 0; pin < 5; pin++ ) {
-        pixels.setPixelColor(pin*2 + 2, Numbers[HighHour][pin][i] * pixels.Color(0,150,0)); 
-        pixels.setPixelColor(pin*2 + 1, Numbers[HighHour][pin][i] * pixels.Color(0,150,0));
+        pixels.setPixelColor(pin, pixels.Color(0, Numbers[HighHour][pin][i] * 150,0)); 
       }
-      pixels.show
+      pixels.show();
       delay(2);
     }
   }
@@ -191,8 +187,7 @@ void loop() {
   for ( int pin = 0; pin < 12; pin++ ) {
       pixels.setPixelColor(pin, 0 );
   }
-  pixels.show;
+  pixels.show();
 
 
 }
-
